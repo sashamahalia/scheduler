fac87948f356f5f7524d07f0f99ed69e620bdacc
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-12 16:26:52 -0700

    Refactor button.js with classnames addon, enable button test.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,21 @@
 import React from "react";
 
 import "components/Button.scss";
+import classnames from 'classnames';
 
 export default function Button(props) {
-   let buttonClass = "button";
+   const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
  
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-    }
-  
-    if (props.danger) {
-      buttonClass += " button--danger";
-    }
- 
-   return <button
-      className={buttonClass}
-      disabled={props.disabled}
-      onClick={props.onClick}
-   >
-      {props.children}
-   </button>;
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
  }
