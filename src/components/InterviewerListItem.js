3a59f5cc42f36fd0cfe2f07d6193a4d050e38983
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-13 13:14:01 -0700

    Create InterviewerListItem component, with corresponding scss and story

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import './InterviewerListItem.scss'
+import classnames from 'classnames'
+
+export default function InterviewerListItem(props) {
+
+
+  const interviewerClass = classnames('interviewers__item', {
+    'interviewers__item--selected': props.selected
+  })
+
+  return ( 
+    <li
+      onClick={() => props.setInterviewer(props.name)}
+      className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+
+}
