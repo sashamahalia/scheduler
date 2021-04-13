12d24c0a51992b824cc11b2dca9eef101654a77f
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-13 16:00:14 -0700

    Create Appointment directory, with index, header, and empty views

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './styles.scss';
+// import Header from './Header';
+
+export default function Appointment() {
+  return (
+    <article className="appointment"></article>
+  )
+}
