82cc259f4efc7df6d9858a78c5f3d7c8b8aa0b6c
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-20 13:59:01 -0700

    Change Application test to use mock axios data

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  render(<Application />);
+
+  const{ getByText } = render(<Application />);
+  return waitForElement(() => getByText("Monday"))
+    .then(() => {
+    fireEvent.click(getByText("Tuesday"))
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  })
 });
