754f008927b04925931419726024fce9c7035dd7
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-20 17:10:08 -0700

    Write test to check creating an appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,17 +1,41 @@
 import React from "react";
 
-import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement, getByAltText, getByText, prettyDOM, getAllByTestId, getByTestId, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+describe('Application', () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
 
-  const{ getByText } = render(<Application />);
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-    fireEvent.click(getByText("Tuesday"))
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  })
+    const{ getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"))
+      
+      fireEvent.click(getByText("Tuesday"))
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it.only("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+    expect(getByText(container, "Archie Cohen")).toBeInTheDocument();
+
+    // const appointments = getAllByTestId(container, "appointment");
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    console.log(prettyDOM(appointment));
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+  });
 });
