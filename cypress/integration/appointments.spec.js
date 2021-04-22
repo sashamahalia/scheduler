53a67e9ac99be63755a0f505b3a701b59b2f262e
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-21 17:33:15 -0700

    Write test for editing appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -3,7 +3,7 @@ describe("Appointments", () => {
     cy.request("GET", "/api/debug/reset");
 
     cy.visit("/");
-    
+
     cy.contains("Monday");
   })
   it("should book an interview", () => {
@@ -20,5 +20,21 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
 
+  });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']").click();
+
+
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
