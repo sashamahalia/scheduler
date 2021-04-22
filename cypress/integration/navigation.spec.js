c37cbec1f72f2580fd5c4ad1a2b6fec8b6d77cf5
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-21 19:04:46 -0700

    Refactor tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,15 +1,13 @@
 const { CYCLIC_KEY } = require("@storybook/addon-actions/dist/constants")
 
-beforeEach(() => {
-  cy.visit("/");
-})
-
 describe("Navigation", () => {
   it("should visit root", () => {
+    cy.visit("/");
   });
   it("should navigate to Tuesday", () => {
+    cy.visit("/");
     cy.contains("[data-testid=day]", "Tuesday")
     .click()
     .should("have.class", "day-list__item--selected")
-  })
+  });
 });
